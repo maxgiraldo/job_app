9d900085f4ea3ded108264ef3fb89b01a9794d21
Max Giraldo <mgiraldo@hearst.com>
2016-12-08 10:55:13 -0500

    Handle POST request with secret password

--- job_app.go
+++ job_app.go
@@ -2,9 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"encoding/json"
+	"net/http"
 )
 
+type SecretMessage struct {
+	Message string
+}
+
 func main() {
-	const msg string = "These violent delights have violent ends."
-	fmt.Printf(msg)
+	http.HandleFunc("/secret_message.json", secretMessage)
+
+	const PORT string = ":8080"
+	fmt.Printf("Listening on port %q\n", PORT)
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
+func secretMessage(w http.ResponseWriter, r *http.Request) { 
+	decoder := json.NewDecoder(r.Body)
+	
+	var accessCode SecretMessage
+	err := decoder.Decode(&accessCode)
+
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	
+	log.Println(accessCode.Message)
+
+	var msg string
+	if accessCode.Message == "JCJRMG" {
+		msg = "These violent delights have violent ends."
+	} else {
+		msg = "That's not the secret password."
+	}
+
+	secretMessage := SecretMessage{msg}
+
+	js, err := json.Marshal(secretMessage)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
